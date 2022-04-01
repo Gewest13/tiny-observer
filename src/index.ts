188f280f7dcb661cc9a7b00efd2181e9aa2e81ed
Ruben van Grinsven <ruben@gewest13.nl>
2022-04-01 12:42:06 +0200

    feat(options): add root

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-export const observe = (el: HTMLElement, inFn: () => void, outFn?: () => void, opts?: { rootMargin?: string, threshold?: number }) => {
+export const observe = (el: HTMLElement, inFn: () => void, outFn?: () => void, opts?: { root?: HTMLElement | null, rootMargin?: string, threshold?: number }) => {
   new IntersectionObserver(([entry]) => {
     if (entry.isIntersecting) inFn();
     else if (outFn) { outFn(); }
   }, { ...opts }).observe(el);
 };
 
-export const observeOnce = (el: HTMLElement, fn: () => void, opts?: { rootMargin?: string, threshold?: number }) => {
+export const observeOnce = (el: HTMLElement, fn: () => void, opts?: { root?: HTMLElement | null, rootMargin?: string, threshold?: number }) => {
   new IntersectionObserver(([entry], observer) => {
     if (entry.isIntersecting) {fn(); observer.unobserve(entry.target);}
   }, { ...opts }).observe(el);
